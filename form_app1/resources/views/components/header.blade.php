246ece4634ae6137caef2c166f254a4eec5f3777
anisonley <anisonley@gmail.com>
2023-05-22 15:38:56 +0100

    Header component

---
+++ form_app1/resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+    <header>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">About</a></li>
+            <li id="contact"><a href="#">Contact Us</a></li>
+        </ul>
+    </header>
+</div>
